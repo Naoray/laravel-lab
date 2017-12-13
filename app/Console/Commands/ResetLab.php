968f3e9f71df347b93221d6b14e2ee1d454d8edf
Naoray Kroaths <krishan.koenig@googlemail.com>
2017-12-13 13:55:31 +0100

    short alias for clearcache option added

--- app/Console/Commands/ResetLab.php
+++ app/Console/Commands/ResetLab.php
@@ -11,7 +11,7 @@ class ResetLab extends Command
      *
      * @var string
      */
-    protected $signature = 'app:reset {-c|--clearcache}';
+    protected $signature = 'app:reset {--C|clearcache}';
 
     /**
      * The console command description.
