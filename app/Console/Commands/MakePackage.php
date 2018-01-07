fb0d7ad36e96723b1d41e71a97252833f29c4342
Naoray <krishan.koenig@googlemail.com>
2018-01-07 19:39:35 +0100

    make makePackage command interactive

--- app/Console/Commands/MakePackage.php
+++ app/Console/Commands/MakePackage.php
@@ -13,17 +13,12 @@ class MakePackage extends Command
      * @var string
      */
     protected $signature = 'make:package 
-                             {name : The name of the package}
-                             {--dir=../packages/ : Directory where the package will be stored}
-                             {--C|copyright=byte5 digital media GmbH : Copyright will be placed in the LICENSE file}
-                             {vendor=byte5digital : Vendor name of the package}
-                             {--A|author=Krishan Koenig : Author of the package}
-                             {--M|mail=kkoenig@byte5.de : Author\'s email address}';
-
-    /**
-     * @var Filesystem
-     */
-    protected $files;
+                             {name? : The name of the package}
+                             {vendor? : Vendor name of the package}
+                             {dir? : Directory where the package will be stored}
+                             {author? : Author of the package}
+                             {mail? : Author\'s email address}
+                             {copyright? : Copyright will be placed in the LICENSE file}';
 
     /**
      * The console command description.
@@ -31,6 +26,47 @@ class MakePackage extends Command
      * @var string
      */
     protected $description = 'Creates a new package and adds it to this lab.';
+    
+    /**
+     * @var Filesystem
+     */
+    protected $files;
+
+    /**
+     * Name of the package.
+     * @var String
+     */
+    protected $packageName;
+
+    /**
+     * Directory where the package will be stored.
+     * @var String
+     */
+    protected $dir;
+
+    /**
+     * Copyright fields which will be placed inside the License.md file.
+     * @var String
+     */
+    protected $copyright;
+
+    /**
+     * The packages vendor name.
+     * @var String
+     */
+    protected $vendor;
+
+    /**
+     * The packages author.
+     * @var String
+     */
+    protected $author;
+
+    /**
+     * The mantainer's email address.
+     * @var String
+     */
+    protected $mail;
 
     /**
      * Create a new command instance.
@@ -52,6 +88,12 @@ class MakePackage extends Command
      */
     public function handle()
     {
+        $this->checkForInputs();
+
+        if (! $this->confirm('Do you wish to continue?')) {
+            return $this->error('Canceled command!');
+        }
+
         $packagePath = $this->getPackagePath();
 
         if ($this->alreadyExists($packagePath)) {
@@ -66,11 +108,31 @@ class MakePackage extends Command
         $this->createServiceProvider($packagePath);
 
         $this->call('package:add', [
-            'name' => $this->getNameInput(),
-            'path' => $this->getDirectoryInput().$this->getNameInput(),
-            'vendor' => $this->getVendorInput(),
+            'name' => $this->packageName,
+            'path' => $this->dir.$this->packageName,
+            'vendor' => $this->vendor,
             '--without-interaction' => true,
         ]);
+    }
+
+    /**
+     * Checks for needed input and prints it out.
+     */
+    public function checkForInputs()
+    {
+        $name = $this->getNameInput();
+        $vendor = $this->getVendorInput();
+        $dir = $this->getDirectoryInput();
+        $author = $this->getAuthorInput();
+        $mail = $this->getMailInput();
+        $copyright = $this->getCopyrightInput();
+
+        $this->table(
+            ['Name', 'Vendor', 'Directory', 'Author', 'E-mail', 'Copyright'],
+            [
+                [$name, $vendor, $dir, $author, $mail, $copyright]
+            ]
+        );
     }
 
     /**
@@ -118,7 +180,7 @@ class MakePackage extends Command
     protected function createServiceProvider($path)
     {
         $this->files->put(
-            $path.'/src/'.$this->getPackageName($this->getNameInput()).'ServiceProvider.php',
+            $path.'/src/'.$this->getPackageName().'ServiceProvider.php',
             $this->buildFile('provider')
         );
         $this->info('Service Provider created successfully!');
@@ -131,11 +193,10 @@ class MakePackage extends Command
      */
     protected function buildFile($name)
     {
-        $packageName = $this->getNameInput();
         $stub = $this->files->get(resource_path().'/stubs/'.$name.'.stub');
 
-        return $this->replaceNamespaces($stub, $packageName)
-            ->replaceNames($stub, $packageName)
+        return $this->replaceNamespaces($stub)
+            ->replaceNames($stub)
             ->replaceCredentials($stub);
     }
 
@@ -146,11 +207,11 @@ class MakePackage extends Command
      * @param  string  $name
      * @return $this
      */
-    protected function replaceNamespaces(&$stub, $name)
+    protected function replaceNamespaces(&$stub)
     {
         $stub = str_replace(
             ['DummyComposerNamespace', 'DummyComposerProviderNamespace', 'DummyRootNamespace'],
-            [$this->getComposerNamespace($name), $this->getComposerProviderNamespace($name), $this->getRootNamespace($name)],
+            [$this->getComposerNamespace(), $this->getComposerProviderNamespace(), $this->getRootNamespace()],
             $stub
         );
 
@@ -164,11 +225,11 @@ class MakePackage extends Command
      * @param  string  $name
      * @return $this
      */
-    protected function replaceNames(&$stub, $name)
+    protected function replaceNames(&$stub)
     {
         $stub = str_replace(
             ['DummyVendorName', 'DummyPackageName', 'DummyClass', 'CompanyOrVendorName'],
-            [$this->getVendorInput(), $name, $this->getPackageName($name).'ServiceProvider', $this->option('copyright')],
+            [$this->vendor, $this->packageName, $this->getPackageName().'ServiceProvider', $this->copyright],
             $stub
         );
 
@@ -186,7 +247,7 @@ class MakePackage extends Command
     {
         $stub = str_replace(
             ['DummyAuthorName', 'DummyAuthorEmail'],
-            [$this->option('author'), $this->option('mail')],
+            [$this->author, $this->mail],
             $stub
         );
 
@@ -197,45 +258,45 @@ class MakePackage extends Command
      * @param $name
      * @return string
      */
-    protected function getRootNamespace($name)
+    protected function getRootNamespace()
     {
-        return ucfirst($this->getVendorInput()).'\\'.$this->getPackageName($name);
+        return ucfirst($this->vendor).'\\'.$this->getPackageName();
     }
 
     /**
      * @param $name
      * @return string
      */
-    protected function getPackageName($name)
+    protected function getPackageName()
     {
-        return ucfirst(camel_case($name));
+        return ucfirst(camel_case($this->packageName));
     }
 
     /**
      * @param $name
      * @return string
      */
-    protected function getComposerNamespace($name)
+    protected function getComposerNamespace()
     {
-        return ucfirst($this->getVendorInput())."\\\\".$this->getNamespace($name);
+        return ucfirst($this->vendor)."\\\\".$this->getNamespace();
     }
 
     /**
      * @param $name
      * @return string
      */
-    protected function getNamespace($name)
+    protected function getNamespace()
     {
-        return $this->getPackageName($name)."\\\\";
+        return $this->packageName."\\\\";
     }
 
     /**
      * @param $name
      * @return string
      */
-    protected function getComposerProviderNamespace($name)
+    protected function getComposerProviderNamespace()
     {
-        return $this->getComposerNamespace($name).$this->getPackageName($name).'ServiceProvider';
+        return $this->getComposerNamespace().$this->getPackageName().'ServiceProvider';
     }
 
     /**
@@ -254,7 +315,7 @@ class MakePackage extends Command
      */
     protected function getPackagePath()
     {
-        return base_path().'/'.$this->getDirectoryInput().$this->getNameInput();
+        return base_path().'/'.$this->dir.$this->packageName;
     }
 
     /**
@@ -264,7 +325,28 @@ class MakePackage extends Command
      */
     protected function getDirectoryInput()
     {
-        return trim($this->option('dir'));
+        if ($this->dir) return $this->dir;
+
+        if (! $this->dir = $this->argument('dir')) {
+            $this->dir = $this->anticipate('Where should the package be installed?', ['../packages/']);
+        }
+
+        return $this->dir;;
+    }
+
+    /**
+     * Get copyright input.
+     * @return String
+     */
+    public function getCopyrightInput()
+    {
+        if ($this->copyright) return $this->copyright;
+
+        if (! $this->copyright = $this->argument('copyright')) {
+            $this->copyright = $this->ask('Who will hold the copyrights?');
+        }
+
+        return $this->copyright;
     }
 
     /**
@@ -274,7 +356,13 @@ class MakePackage extends Command
      */
     protected function getNameInput()
     {
-        return trim($this->argument('name'));
+        if ($this->packageName) return $this->packageName;
+
+        if (! $this->packageName = trim($this->argument('name'))) {
+            $this->packageName = $this->ask('What\'s your packages name?');
+        }
+
+        return $this->packageName;
     }
 
     /**
@@ -284,6 +372,43 @@ class MakePackage extends Command
      */
     protected function getVendorInput()
     {
-        return trim($this->argument('vendor'));
+        if ($this->vendor) return $this->vendor;
+        
+        if (! $this->vendor = trim($this->argument('vendor'))) {
+            $this->vendor = $this->ask('What\'s the packages github name (vendor name of the package)?');
+        }
+        // dd($this->vendor);
+
+        return $this->vendor;
+    }
+
+    /**
+     * Get the author name input.
+     * @return String
+     */
+    public function getAuthorInput()
+    {
+        if ($this->author) return $this->author;
+
+        if (! $this->author = $this->argument('author')) {
+            $this->author = $this->ask('Who is the author of the package?');
+        }
+
+        return $this->author;
+    }
+
+    /**
+     * Get mail input.
+     * @return String
+     */
+    public function getMailInput()
+    {
+        if ($this->mail) return $this->mail;
+
+        if (! $this->mail = $this->argument('mail')) {
+            $this->mail = $this->ask('What\'s the mantainer\'s e-mail?');
+        }
+
+        return $this->mail;
     }
 }
