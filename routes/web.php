f9177c1c7ac3098424f6d9e5d1b96eb5584fafce
Naoray Kroaths <krishan.koenig@googlemail.com>
2018-02-08 10:50:10 +0100

    upgrade to laravel 5.6

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,7 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
