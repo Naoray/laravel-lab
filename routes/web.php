05bad8580348e36c3a8f6a8fa2a95e4f4ee0e6c0
Naoray Kroaths <krishan.koenig@googlemail.com>
2018-03-13 08:49:49 +0100

    remove double auth and home routes

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,3 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
-
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
