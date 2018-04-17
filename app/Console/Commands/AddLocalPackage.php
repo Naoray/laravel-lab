80481b1afe18b9fa4853970f77869b3dd7637cde
Naoray Kroaths <krishan.koenig@googlemail.com>
2018-04-17 14:18:17 +0200

    ask for package branch to use

--- app/Console/Commands/AddLocalPackage.php
+++ app/Console/Commands/AddLocalPackage.php
@@ -6,61 +6,70 @@ use Illuminate\Console\Command;
 
 class AddLocalPackage extends Command
 {
-    /**
-     * The name and signature of the console command.
-     *
-     * @var string
-     */
-    protected $signature = 'package:add {name?} {path?} {vendor?} {--type=path} {--without-interaction}';
+	/**
+	 * The name and signature of the console command.
+	 *
+	 * @var string
+	 */
+	protected $signature = 'package:add {name?} {path?} {vendor?} {branch?} {--type=path} {--without-interaction}';
 
-    /**
-     * The console command description.
-     *
-     * @var string
-     */
-    protected $description = 'Adds local package with repository option.';
+	/**
+	 * The console command description.
+	 *
+	 * @var string
+	 */
+	protected $description = 'Adds local package with repository option.';
 
-    /**
-     * Create a new command instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        parent::__construct();
-    }
+	/**
+	 * Create a new command instance.
+	 *
+	 * @return void
+	 */
+	public function __construct()
+	{
+		parent::__construct();
+	}
 
-    /**
-     * Execute the console command.
-     *
-     * @return mixed
-     */
-    public function handle()
-    {
-        $name = $this->argument('name');
-        $path = $this->argument('path');
-        $vendor = $this->argument('vendor');
-        $type = $this->option('type');
+	/**
+	 * Execute the console command.
+	 *
+	 * @return mixed
+	 */
+	public function handle()
+	{
+		$name = $this->argument('name');
+		$path = $this->argument('path');
+		$vendor = $this->argument('vendor');
+		$branch = $this->argument('branch');
+		$type = $this->option('type');
 
-        if (! $vendor) {
-            $vendor = $this->ask('What is your package\'s vendor name?');
-        }
+		if (! $vendor) {
+			$vendor = $this->ask('What is your package\'s vendor name?');
+		}
 
-        if (! $name) {
-            $name = $this->ask('What is your package\'s name?');
-        }
+		if (! $name) {
+			$name = $this->ask('What is your package\'s name?');
+		}
 
-        if (! $path) {
-            $path = $this->anticipate('What is your package\'s path?', ['../packages/'.$name]);
-        }
+		if (! $path) {
+			$path = $this->anticipate('What is your package\'s path?', ['../packages/'.$name]);
+		}
 
-        $this->table(['vendor', 'name', 'path', 'type'], [[$vendor, $name, $path, $type]]);
-        if (! $this->option('without-interaction') && ! $this->confirm('Do you wish to continue?')) {
-            return;
-        }
+		if (! $branch) {
+			$branch = $this->anticipate('What branch do you want to link?', ['dev', 'master']);
+		}
 
-        exec('composer config repositories.'.$name.' '.$type.' '.$path);
-        sleep(1);
-        exec('composer require "'.$vendor.'/'.$name.':dev-master"');
-    }
+		$this->table(['vendor', 'name', 'path', 'branch', 'type'], [[$vendor, $name, $path, $branch, $type]]);
+		if (! $this->option('without-interaction') && ! $this->confirm('Do you wish to continue?')) {
+			return;
+		}
+
+		if ($branch === 'dev' || $branch === 'master') {
+			$branch = 'dev-'.$branch;
+		}
+
+		exec('composer config repositories.'.$name.' '.$type.' '.$path);
+		sleep(1);
+		exec('composer require "'.$vendor.'/'.$name.':'.$branch.'"');
+	}
 }
